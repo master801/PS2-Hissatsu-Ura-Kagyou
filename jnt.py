65c8fe9404a62e6da3c7b492809686b23824f3c9
Master <master801@users.noreply.github.com>
2024-03-07 22:34:08 -0600

    Initial work on create
    Signed-off-by: Master <master801@users.noreply.github.com>

--- jnt.py
+++ jnt.py
@@ -4,6 +4,7 @@
 import dataclasses
 import typing
 import struct
+import yaml
 
 
 @dataclasses.dataclass
@@ -15,6 +16,7 @@ class JNT:
         @dataclasses.dataclass
         class Entry:
 
+            pos: int
             offset: int
             size: int  # length / 16 (4 int blocks)
 
@@ -33,13 +35,14 @@ class JNT:
                 _io.seek(self.offset)
                 data: bytes = _io.read(size)
                 _io.seek(seek)
+                del seek
                 return data
 
         @dataclasses.dataclass
         class Name:
 
             offset: int
-            fn: str = None
+            fp: str = None
 
             def read(self, _io: typing.BinaryIO):
                 seek = _io.tell()  # save seek position
@@ -49,7 +52,7 @@ class JNT:
                     buf += _io.read(1)
                     continue
                 _io.seek(seek)  # jump back to seek position
-                self.fn = buf[:-1].decode(encoding='ascii')  # decode file name
+                self.fp = buf[:-1].decode(encoding='ascii')  # decode file name
                 return self
 
         size: int  # size
@@ -59,6 +62,55 @@ class JNT:
 
     header: Header = None
     entries: list[Entry] = None
+
+    def write_directory_listing(self, io_jnt: typing.BinaryIO, directory_listing: list[str]):
+        # TODO
+        # none of this shit makes sense
+        return
+
+    def write_entries(self, io_jnt: typing.BinaryIO, directory_listing: list[str]):
+        self.write_directory_listing(io_jnt, directory_listing)
+        return
+
+    # noinspection PyTypeChecker
+    def create_header(self):
+        self.header = JNT.Header(
+            [
+                # always 0x800?
+                JNT.Header.Entry(0, 0x800, None),  # size of header entries
+                JNT.Header.Entry(8, 0x800, None)  # offset to first JNT file data?
+            ]
+        )
+        return
+
+    # noinspection PyComparisonWithNone
+    def write_header(self, _io: typing.BinaryIO):
+        if self.header == None:
+            self.create_header()  # generate a dummy header first
+            pass
+
+        for i in self.header.header_entries:
+            size = i.size
+            offset = i.offset
+            if size == None:
+                size = 0xFFFF  # write FFFF as placeholder
+                pass
+            if offset == None:
+                offset = 0xFFFF  # write FFFF as placeholder
+                pass
+            _io.write(struct.pack('<I', offset))
+            _io.write(struct.pack('<I', size))
+            del offset
+            del size
+            continue
+        del i
+
+        while _io.tell() != 0x800:  # pad header to 0x800
+            _io.write(b'\x00')
+            pass
+
+        # TODO Re-write actual size / offset of header entries
+        return
 
     # noinspection PyMethodMayBeStatic
     def read_entry(self, _io: typing.BinaryIO) -> Entry:
@@ -76,7 +128,7 @@ class JNT:
         for i in range(0, len(self.entries)):
             entry = self.read_entry(_io)
             self.entries[i] = entry
-            print(f'Found file entry \"{entry.name.fn}\" at offset 0x{entry.data.offset:02X}')
+            print(f'Found file entry \"{entry.name.fp}\" at offset 0x{entry.data.offset:02X}')
             continue
         del i
         return
@@ -84,8 +136,8 @@ class JNT:
     def read_header(self, _io: typing.BinaryIO):
         self.header = JNT.Header(
             [
-                JNT.Header.Entry(struct.unpack('<I', _io.read(4))[0], struct.unpack('<I', _io.read(4))[0]),
-                JNT.Header.Entry(struct.unpack('<I', _io.read(4))[0], struct.unpack('<I', _io.read(4))[0])  # Is this an actual second entry? Doesn't look like it is...
+                JNT.Header.Entry(_io.tell(), struct.unpack('<I', _io.read(4))[0], struct.unpack('<I', _io.read(4))[0]),
+                JNT.Header.Entry(_io.tell(), struct.unpack('<I', _io.read(4))[0], struct.unpack('<I', _io.read(4))[0])  # Is this an actual second entry? Doesn't look like it is... just looks like an offset to the first entry
             ]
         )
         return
--- main.py
+++ main.py
@@ -1,14 +1,146 @@
 #!/usr/bin/env python3
 # Created by Master on 2/27/2024
 
+import io
 import os
 import pathlib
 import argparse
+import typing
+import yaml
 
 import jnt
 
 
-USE_PADDED: bool = True
+USE_PADDED: bool = False
+EXTRACT_SUB_FILES: bool = False
+YAML_FILE_LISTING: str = 'DO_NOT_EDIT.YAML'
+
+
+def create(folder_input: str, file_output: str):
+    if not os.path.exists(folder_input):
+        print(f'Input folder \"{folder_input}\" does not exist!')
+        return
+    else:
+        if not os.path.isdir(folder_input):
+            print(f'Given input \"{folder_input}\" is not a folder?!')
+            return
+        pass
+    if os.path.exists(file_output) and os.path.isdir(file_output):
+        print(f'Output \"{file_output}\" is a directory?! This should not happen!')
+        return
+
+    # TODO
+    # generate file list
+    fp_directory_listing = os.path.join(folder_input, YAML_FILE_LISTING)
+    if not os.path.exists(fp_directory_listing):
+        print(f'Directory listing \"{fp_directory_listing}\" does not exist!\nPlease re-extract \"DATA.JNT\"!')
+        return
+    with open(fp_directory_listing, mode='rt+') as io_file_listing:
+        directory_listing = yaml.safe_load(stream=io_file_listing)
+        pass
+    for i in directory_listing:  # ensure ALL of these files exist
+        tp = os.path.join(folder_input, i)
+        if not os.path.exists(tp):
+            print(f'File \"{i}\" does not exist in the path, but exists in the directory listing YAML!')
+            print('Please re-extract \"DATA.JNT\"!')
+            return
+        del tp
+        continue
+    del i
+
+    file_jnt: jnt.JNT = jnt.JNT()
+    with io.BytesIO() as io_jnt:
+        file_jnt.write_header(io_jnt)
+        file_jnt.write_entries(io_jnt, directory_listing)
+
+        io_jnt.seek(0)  # reset pos before writing entire file to disk
+        if os.path.exists(file_output):
+            mode = 'w+'
+            pass
+        else:
+            mode = 'x'
+            pass
+        with open(file_output, mode=f'{mode}b') as io_new_jnt:
+            io_new_jnt.write(
+                io_jnt.read()
+            )
+            pass
+        pass
+    return
+
+
+def generate_file_listing(file_jnt: jnt.JNT, stream):
+    file_listing = []
+    for entry in file_jnt.entries:
+        file_listing.append(
+            entry.name.fp
+        )
+        # TODO generate sub-jnt files too
+        continue
+    return yaml.safe_dump(file_listing, stream=stream, encoding='utf8')
+
+
+def extract_io(io_input_jnt: typing.BinaryIO, folder_extracted: str):
+    if not os.path.exists(folder_extracted):
+        os.makedirs(folder_extracted)
+        pass
+
+    file_jnt: jnt.JNT = jnt.JNT()
+    file_jnt.read_from_file(io_input_jnt)
+
+    fp_file_listing = os.path.join(folder_extracted, YAML_FILE_LISTING)
+    if os.path.exists(fp_file_listing):
+        mode = 'w+'
+        pass
+    else:
+        mode = 'x'
+        pass
+    with open(fp_file_listing, mode=f'{mode}t', encoding='utf-8') as io_file_listing:
+        generate_file_listing(file_jnt, io_file_listing)
+        pass
+
+    for entry in file_jnt.entries:
+        fp = pathlib.Path(os.path.join(folder_extracted, entry.name.fp))
+        if not fp.parent.exists():
+            os.makedirs(fp.parent)
+            pass
+
+        if EXTRACT_SUB_FILES and (fp.name.endswith('.JNT')):  # extract sub-JNT files
+            if os.path.exists(fp.name):
+                if os.path.isfile(fp.name):
+                    print(f'Cannot extract file \"{fp}\"!')
+                    continue
+                pass
+            with io.BytesIO(entry.data.read(io_input_jnt, entry.size_padded)) as io_sub_jnt:
+                extract_io(io_sub_jnt, fp.as_posix())
+                pass
+            del io_sub_jnt
+            pass
+        else:
+            if fp.exists():
+                mode = 'w+'
+                pass
+            else:
+                mode = 'x'
+                pass
+            # TODO UNCOMMENT
+            with open(fp, mode=f'{mode}b') as io_entry:
+                size: int = entry.size
+                if USE_PADDED:
+                    size = entry.size_padded
+                    pass
+                data: bytes = entry.data.read(io_input_jnt, size)
+                io_entry.write(data)
+                del data
+                del size
+                pass
+            del io_entry
+            del mode
+            pass
+
+        del fp
+        continue
+    return
 
 
 def extract(input_jnt: str, folder_extracted: str):
@@ -20,40 +152,10 @@ def extract(input_jnt: str, folder_extracted: str):
     else:
         print(f'Input file \"{input_jnt}\" does not exist!')
         pass
-    if not os.path.exists(folder_extracted):
-        os.makedirs(folder_extracted)
-        pass
 
     print(f'Reading file \"{input_jnt}\"...')
     with open(input_jnt, mode='rb+') as io_jnt:
-        file_jnt = jnt.JNT()
-        file_jnt.read_from_file(io_jnt)
-        for entry in file_jnt.entries:
-            fp = pathlib.Path(os.path.join(folder_extracted, entry.name.fn))
-            if not fp.parent.exists():
-                os.makedirs(fp.parent)
-                pass
-            if fp.exists():
-                mode = 'w+'
-                pass
-            else:
-                mode = 'x'
-                pass
-
-            with open(fp, mode=f'{mode}b') as io_entry:
-                size: int = entry.size
-                if USE_PADDED:
-                    size = entry.size_padded
-                    pass
-                data: bytes = entry.data.read(io_jnt, size)
-                io_entry.write(data)
-                del data
-                del size
-                pass
-            del io_entry
-
-            del fp
-            continue
+        extract_io(io_jnt, folder_extracted)
         pass
     print('Done reading file')
     return
@@ -72,7 +174,7 @@ def main():
     if args.create:
         # TODO
         print('Creating...')
-        print('')
+        create(args.input, args.output)
         print('Done creating')
         pass
     elif args.extract:
